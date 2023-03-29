8a9c692543fa15233d0c38f65bc4f14388a9c08d
hora459 <modysamir459@gmail.com>
2023-03-29 16:34:20 +0200

    :)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
 import Heaps.IHeap;
 import Heaps.MaxHeap;
-import Trees.AvlTree;
-import Trees.ITree;
-import Trees.Node;
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static Trees.BInarySearchTree.printTree;
 
 public class Main {
     public static void main(String[] args) {
